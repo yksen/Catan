271fbc15a1422f9668c87ea4054bb4a837d52d7e
yksen <selegakamil@gmail.com>
2021-11-16 15:02:20 +0100

    Tile borders

--- src/Catan.hpp
+++ src/Catan.hpp
@@ -19,17 +19,21 @@ class Catan {
         Stage stage;
         Map map;  
 
-        sf::Texture textures;
+        sf::Texture tileTextures, diceTextures;
         int textureSize = 256;
 
-        int tileSize = 90;
-        int tileWidth = tileSize * sqrt(3);
-        int tileHeight =  tileSize * 2;
+        int bgTileSize = 90;
+        int bgTileWidth = bgTileSize * sqrt(3);
+        int bgTileHeight =  bgTileSize * 2;
+        
+        int borderSize = 5;    
+        int tileSize = bgTileSize - borderSize;
 
     public:        
         Catan(sf::RenderWindow* x) {
             window = x;
-            textures.loadFromFile("../assets/textures.png");
+            tileTextures.loadFromFile("../assets/textures.png");
+            diceTextures.loadFromFile("../assets/dice_faces.png");
             stage = menu;
         }
         
@@ -49,15 +53,23 @@ class Catan {
             {
                 for (size_t x = 0; x < map.width; x++)
                 {
+                    sf::CircleShape bgTile(bgTileSize, 6);
                     sf::CircleShape tile(tileSize, 6);
-                    sf::Vector2f position(x * tileWidth + ((y + 1) % 2) * (tileWidth / 2), y * (3 * (tileHeight / 4)));
                     auto type = map.tileMap[x][y].type;
 
+                    bgTile.setOrigin(bgTileSize, bgTileSize);
+                    tile.setOrigin(tileSize, tileSize);
+
+                    sf::Vector2f position(bgTileWidth + x * bgTileWidth + ((y + 1) % 2) * (bgTileWidth / 2), bgTileHeight + y * (3 * (bgTileHeight / 4)));
+                    bgTile.setPosition(position);
                     tile.setPosition(position);
-                    // tile.setFillColor(TileColors[type]);
-                    tile.setTexture(&textures);
+                    
+                    // bgTile.setFillColor(TileColors[type]);
+                    bgTile.setFillColor(sf::Color::White);
+                    tile.setTexture(&tileTextures);
                     tile.setTextureRect(sf::IntRect((type % 4) * textureSize, ((type / 4) % 4) * textureSize, textureSize, textureSize));
                 
+                    window->draw(bgTile);
                     window->draw(tile);
                 }
             }
