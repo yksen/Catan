3e5943c8eef74f23bf6d2fbecfc024908266cb39
yksen <selegakamil@gmail.com>
2021-11-16 17:27:12 +0100

    Moved classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "Catan.hpp"
+#include "classes/Catan.hpp"
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
@@ -10,7 +10,7 @@ int main()
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Catan", sf::Style::Fullscreen, settings);
 
-    Catan game(&window);
+    Catan game(&window, window.getSize());
     game.setMap("Standard");
     
     while (window.isOpen())
