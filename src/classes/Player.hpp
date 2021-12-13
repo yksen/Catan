3ce1902c2a13c1613db4d3246fe920e960d5eb9c
yksen <selegakamil@gmail.com>
2021-12-13 23:42:59 +0100

    Added player IDs

--- src/classes/Player.hpp
+++ src/classes/Player.hpp
@@ -13,21 +13,24 @@ enum Resource
     wool
 };
 
+int commonId = 0;
+
 class Player
 {
 public:
+    int id;
     std::string name;
     sf::Color color;
     int points = 0;
     std::map<Resource, int> resources{
-        {brick, 999}, {lumber, 999}, {ore, 999}, {grain, 999}, {wool, 999}
-    };
+        {brick, 999}, {lumber, 999}, {ore, 999}, {grain, 999}, {wool, 999}};
 
     Player()
     {
     }
     Player(std::string name, sf::Color color)
     {
+        this->id = commonId++;
         this->name = name;
         this->color = color;
     }
